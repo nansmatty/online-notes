218d239b631aed3795f5a23fb4ab1d058c5b7c1c
Narayan Maity <41020595+nansmatty@users.noreply.github.com>
2018-07-09 23:02:34 +0530

    Update mainpageloggedin.php

--- mainpageloggedin.php
+++ mainpageloggedin.php
@@ -16,7 +16,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>My Notes</title>
-    <link href="css/bootstrap/bootstrap.css" rel="stylesheet">
+    <link href="bootstrap.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Arvo" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="style.css">
     <style type="text/css">
@@ -124,8 +124,8 @@
       </div>
     </div>
 
-    <script src="js/jquery-3.3.1.min.js"></script>
-    <script src="js/bootstrap.js"></script>
+    <script src="jquery-3.3.1.min.js"></script>
+    <script src="bootstrap.js"></script>
     <script src="mynotes.js"></script>
   </body>
 </html>
